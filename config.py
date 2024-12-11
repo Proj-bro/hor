130d5323b675da89ce5b295c4d4db633f423c615
Proj-bro <proj32164@gmail.com>
2024-12-11 23:43:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,23 @@
 from decouple import config
 
-
 class Config(object):
     SECRET_KEY = config('SECRET_KEY', default='guess-me')
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-
+    PORT = config('PORT', default=5000, cast=int)  # Add port setting here
 
 class ProductionConfig(Config):
     DEBUG = False
     MAIL_DEBUG = False
 
-
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
 
-
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-
 
 class TestingConfig(Config):
     TESTING = True
